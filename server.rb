4dad081f589043eea20f07aa4827d0bf817d62f9
Stefan Sundin <git@stefansundin.com>
2017-11-12 00:50:09 -0800

    Allow for multiple tunnels.

--- server.rb
+++ server.rb
@@ -3,40 +3,60 @@ require "socket"
 require "net/ssh"
 require "net/ssh/proxy/jump"
 
-listen_port = 9090
-upstream_port = 8880
-
-host = "example.com"
-user = "my_username"
-proxy_host = "bastion.example.com"
-
-proxy = Net::SSH::Proxy::Jump.new("#{user}@#{proxy_host}")
-ssh = Net::SSH.start(host, user, proxy: proxy)
-local_port = ssh.forward.local(0, "127.0.0.1", upstream_port)
-puts "ephemeral port: #{local_port} -> #{upstream_port}"
+tunnels = [
+  {
+    local_interface: "127.0.0.1", # This tunnel only accepts connections from your own computer
+    local_port: 8881,
+    remote_host: "127.0.0.1",
+    remote_port: 8880,
+    user: "my_username",
+    host: "dev1.example.com",
+    proxy_jump: "my_username@bastion.example.com",
+  },
+  {
+    local_interface: nil, # This tunnel accepts connections on all interfaces
+    local_port: 8882,
+    remote_host: "127.0.0.1",
+    remote_port: 8880,
+    user: "my_username",
+    host: "dev2.example.com",
+    proxy_jump: "my_username@bastion.example.com",
+  },
+]
 
 run_ssh_thread = true
-ssh_thread = Thread.new do
-  while run_ssh_thread do
-    ssh.process(0.01)
-    Thread.pass
+
+tunnels.each do |tunnel|
+  proxy = Net::SSH::Proxy::Jump.new(tunnel[:proxy_jump])
+  tunnel[:ssh] = Net::SSH.start(tunnel[:host], tunnel[:user], proxy: proxy)
+  tunnel[:forwarded_port] = tunnel[:ssh].forward.local(0, tunnel[:remote_host], tunnel[:remote_port])
+  puts "Opened SSH connection to #{tunnel[:proxy_jump]} -> #{tunnel[:host]}:#{tunnel[:remote_port]} with ephemeral port #{tunnel[:forwarded_port]}"
+
+  tunnel[:thread] = Thread.new do
+    while run_ssh_thread do
+      tunnel[:ssh].process(0.01)
+      Thread.pass
+    end
   end
+
+  tunnel[:server_socket] = TCPServer.new(tunnel[:local_interface], tunnel[:local_port])
+  tunnel[:server_socket].listen(128)
+  tunnel[:conns] = []
 end
 
-open_conns = []
-server_socket = TCPServer.new(listen_port)
-server_socket.listen(128)
 loop do
-  read_sockets = [server_socket, open_conns].flatten
-  result = IO.select(read_sockets)
+  sockets = tunnels.map { |t| [t[:server_socket], t[:conns]] }.flatten
+  result = IO.select(sockets)
   result[0].each do |sock|
-    if sock == server_socket
-      client_socket = server_socket.accept
-      upstream_socket = TCPSocket.new("localhost", local_port)
-      open_conns.push([client_socket, upstream_socket])
-      puts "/"
-    else
-      open_conns.each do |conn|
+    tunnels.each do |tunnel|
+      if sock == tunnel[:server_socket]
+        client_socket = tunnel[:server_socket].accept
+        upstream_socket = TCPSocket.new("localhost", tunnel[:forwarded_port])
+        tunnel[:conns].push([client_socket, upstream_socket])
+        puts
+        puts "New connection to #{tunnel[:proxy_jump]} -> #{tunnel[:host]}:#{tunnel[:remote_port]}"
+      end
+      tunnel[:conns].each do |conn|
         begin
           if sock == conn[0]
             conn[1].write(conn[0].read_nonblock(4096))
@@ -48,7 +68,7 @@ loop do
         rescue EOFError
           conn[0].close
           conn[1].close
-          open_conns.delete(conn)
+          tunnel[:conns].delete(conn)
           puts "!"
           break
         end
@@ -56,7 +76,9 @@ loop do
     end
   end
 end
-server_socket.close
 
 run_ssh_thread = false
-ssh_thread.join
+tunnels.each do |tunnel|
+  tunnel[:server_socket].close
+  tunnel[:thread].join
+end
