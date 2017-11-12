193897abd51d6c6d2f01497dfd29d8b973c10c95
Stefan Sundin <git@stefansundin.com>
2017-11-11 23:42:58 -0800

    Add net-ssh to run the SSH port forwarding.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "net-ssh"
+gem "rbnacl", "< 5.0", ">= 3.2.0"
+gem "rbnacl-libsodium"
+gem "bcrypt_pbkdf"
--- server.rb
+++ server.rb
@@ -1,8 +1,27 @@
 #!/usr/bin/env ruby
 require "socket"
+require "net/ssh"
+require "net/ssh/proxy/jump"
 
 listen_port = 9090
-upstream_port = 8881
+upstream_port = 8880
+
+host = "example.com"
+user = "my_username"
+proxy_host = "bastion.example.com"
+
+proxy = Net::SSH::Proxy::Jump.new("#{user}@#{proxy_host}")
+ssh = Net::SSH.start(host, user, proxy: proxy)
+local_port = ssh.forward.local(0, "127.0.0.1", upstream_port)
+puts "ephemeral port: #{local_port} -> #{upstream_port}"
+
+run_ssh_thread = true
+ssh_thread = Thread.new do
+  while run_ssh_thread do
+    ssh.process(0.01)
+    Thread.pass
+  end
+end
 
 open_conns = []
 server_socket = TCPServer.new(listen_port)
@@ -13,18 +32,18 @@ loop do
   result[0].each do |sock|
     if sock == server_socket
       client_socket = server_socket.accept
-      puts "/"
-      upstream_socket = TCPSocket.new("localhost", upstream_port)
+      upstream_socket = TCPSocket.new("localhost", local_port)
       open_conns.push([client_socket, upstream_socket])
+      puts "/"
     else
       open_conns.each do |conn|
         begin
           if sock == conn[0]
-            printf "."
             conn[1].write(conn[0].read_nonblock(4096))
-          elsif sock == conn[1]
             printf "."
+          elsif sock == conn[1]
             conn[0].write(conn[1].read_nonblock(4096))
+            printf "."
           end
         rescue EOFError
           conn[0].close
@@ -38,3 +57,6 @@ loop do
   end
 end
 server_socket.close
+
+run_ssh_thread = false
+ssh_thread.join
